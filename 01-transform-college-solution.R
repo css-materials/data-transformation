7cdd14d835e309c65ecc0fd4a347b8903d36b8f0
Sabrina Nardin <nardin@uchicago.com>
2022-10-06 13:56:32 -0500

    Updated in-class data transformation practice exercises

--- 01-transform-college-solution.R
+++ 01-transform-college-solution.R
@@ -1,3 +1,7 @@
+# practice transforming data - solutions
+# check the dplyr documentation https://dplyr.tidyverse.org/reference/index.html
+
+
 # load required packages
 library(tidyverse)
 library(rcis)
@@ -9,46 +13,51 @@ glimpse(scorecard)
 # view help file for the dataset
 ?scorecard
 
+
 # generate a data frame of schools with a greater than
 # 40% share of first-generation students
 filter(.data = scorecard, firstgen > .40)
 
-# generate a data frame with the 10 most expensive (net cost) colleges in 2018-19
+
+# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
+# hint: use arrange and slice to sort the data from most to least expensive (desc), keep the first 10
 arrange(.data = scorecard, desc(netcost)) %>%
   slice(1:10)
-
+# alternative solution using slice_max() function
 slice_max(.data = scorecard, order_by = netcost, n = 10)
+
 
 # generate a data frame with the average SAT score for each type of college
 scorecard %>%
   group_by(type) %>%
   summarize(mean_sat = mean(satavg, na.rm = TRUE))
 
-# calculate for each school how many students it takes to pay
-# the average faculty member's salary and generate a data frame
-# with the school's name and the calculated value
-# - use net cost of attendance
+
+# calculate for each school how many students it takes to pay the average faculty
+# member's salary and generate a data frame with the school's name and the calculated value
+# use net cost of attendance
 scorecard %>%
   mutate(ratio = avgfacsal / netcost) %>%
   select(name, ratio)
 
-# calculate how many private, nonprofit schools have a smaller net cost
-# than Cornell University
-## Hint: the result should be a data frame with one row for Cornell University,
-## and a column containing the requested value.
 
-## report the number as the total number of schools
+# Bonus: calculate how many private, nonprofit schools have a smaller net cost
+# than the "University of Chicago": total number of schools and percentage.
+# hint: the result should be a data frame with one row for Cornell University,
+# and a column containing the requested value.
+
+## code for the number as the total number of schools
 scorecard %>%
   filter(type == "Private, nonprofit") %>%
   arrange(netcost) %>%
-  # use row_number() but subtract 1 since Cornell is not cheaper than itself
+  # use row_number() but subtract 1 since UChicago is not cheaper than itself
   mutate(school_cheaper = row_number() - 1) %>%
-  filter(name == "Cornell University") %>%
+  filter(name == "University of Chicago") %>%
   glimpse()
 
-## Report the number as the percentage of schools
+## code for the number as the percentage of schools
 scorecard %>%
   filter(type == "Private, nonprofit") %>%
   mutate(netcost_rank = percent_rank(netcost)) %>%
-  filter(name == "Cornell University") %>%
+  filter(name == "University of Chicago") %>%
   glimpse()
--- 01-transform-college.R
+++ 01-transform-college.R
@@ -1,4 +1,6 @@
 # practice transforming data
+# check the dplyr documentation https://dplyr.tidyverse.org/reference/index.html
+
 
 # load required packages
 library(tidyverse)
@@ -11,38 +13,39 @@ glimpse(scorecard)
 # view help file for the dataset
 ?scorecard
 
+
 # generate a data frame of schools with a greater than
 # 40% share of first-generation students
 filter(.data = ________, ________)
 
-# generate a data frame with the 10 most expensive (net cost) colleges in 2018-19
+
+# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
+# hint: use arrange and slice to sort the data from most to least expensive (desc), keep the first 10
 arrange(.data = ________, ________) %>%
   slice(________)
-
+# alternative solution using slice_max() function
 slice_max(.data = ________, order_by = ________, n = ________)
+
 
 # generate a data frame with the average SAT score for each type of college
 scorecard %>%
   group_by(________) %>%
-  summarize(mean_sat = ________)
+  summarize(mean_sat = ______________________)
 
-# calculate for each school how many students it takes to pay
-# the average faculty member's salary and generate a data frame
-# with the school's name and the calculated value
-# - use net cost of attendance
+
+# calculate for each school how many students it takes to pay the average faculty
+# member's salary and generate a data frame with the school's name and the calculated value
+# use net cost of attendance
 scorecard %>%
-  mutate(________ = ________) %>%
+  mutate(________ = _______ / _________) %>%
   select(________, ________)
 
-# calculate how many private, nonprofit schools have a smaller net cost
-# than Cornell University
-## Hint: the result should be a data frame with one row for Cornell University,
-## and a column containing the requested value.
 
-## report the number as the total number of schools
-scorecard %>%
-  ________ %>% ...
+# Bonus: calculate how many private, nonprofit schools have a smaller net cost
+# than the "University of Chicago": total number of schools and percentage.
+# hint: the result should be a data frame with one row for Cornell University,
+# and a column containing the requested value.
 
-## Report the number as the percentage of schools
-scorecard %>%
-  ________ %>% ...
+## code for the number as the total number of schools
+
+## code for the number as the percentage of schools
