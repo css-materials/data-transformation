84db21997d083acd77bde6ae0d6b794ae52055c0
Sabrina Nardin <nardin@uchicago.com>
2023-10-09 09:02:17 -0500

    updated exercises

--- 01-transform-college-solution.R
+++ 01-transform-college-solution.R
@@ -1,14 +1,17 @@
-# practice transforming data - solutions
-# check the dplyr documentation https://dplyr.tidyverse.org/reference/index.html
+# PRACTICE TRANSFORMING DATA WITH DPLYR and PIPES
+# dplyr documentation https://dplyr.tidyverse.org/reference/index.html
 
 
-# load required packages
+# load required packages (if you are using R locally, install them first)
 library(tidyverse)
 library(rcis)
 
-# view header of the dataset
+# view header of the dataset, look at the variables (and their names)
 scorecard
-glimpse(scorecard)
+glimpse(scorecard) 
+
+# compare head() with glimpse()
+head(scorecard)
 
 # view help file for the dataset
 ?scorecard
@@ -19,45 +22,26 @@ glimpse(scorecard)
 filter(.data = scorecard, firstgen > .40)
 
 
-# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
-# hint: use arrange and slice to sort the data from most to least expensive (desc), keep the first 10
-arrange(.data = scorecard, desc(netcost)) %>%
-  slice(1:10)
-# alternative solution using slice_max() function
-slice_max(.data = scorecard, order_by = netcost, n = 10)
-
-
 # generate a data frame with the average SAT score for each type of college
 scorecard %>%
   group_by(type) %>%
   summarize(mean_sat = mean(satavg, na.rm = TRUE))
 
 
-# calculate for each school how many students it takes to pay the average faculty
-# member's salary and generate a data frame with the school's name and the calculated value
+# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
+# HINT: use arrange(), sort the data from most to least expensive, keep the first 10
+arrange(.data = scorecard, desc(netcost)) %>%
+  head(10)
+# alternative solution with slice()
+arrange(.data = scorecard, desc(netcost)) %>%
+  slice(1:10)
+# alternative solution using slice_max()
+slice_max(.data = scorecard, order_by = netcost, n = 10)
+
+
+# for each school calculate how many students it takes to pay the average faculty salary
+# and generate a data frame with the school's name and the calculated value
 # use net cost of attendance
 scorecard %>%
   mutate(ratio = avgfacsal / netcost) %>%
   select(name, ratio)
-
-
-# Bonus: calculate how many private, nonprofit schools have a smaller net cost
-# than the "University of Chicago": total number of schools and percentage.
-# hint: the result should be a data frame with one row for the "University of Chicago",
-# and a column containing the requested value.
-
-## code for the number as the total number of schools
-scorecard %>%
-  filter(type == "Private, nonprofit") %>%
-  arrange(netcost) %>%
-  # use row_number() but subtract 1 since UChicago is not cheaper than itself
-  mutate(school_cheaper = row_number() - 1) %>%
-  filter(name == "University of Chicago") %>%
-  glimpse()
-
-## code for the number as the percentage of schools
-scorecard %>%
-  filter(type == "Private, nonprofit") %>%
-  mutate(netcost_rank = percent_rank(netcost)) %>%
-  filter(name == "University of Chicago") %>%
-  glimpse()
--- 01-transform-college.R
+++ 01-transform-college.R
@@ -1,14 +1,17 @@
-# practice transforming data
-# check the dplyr documentation https://dplyr.tidyverse.org/reference/index.html
+# PRACTICE TRANSFORMING DATA WITH DPLYR and PIPES
+# dplyr documentation https://dplyr.tidyverse.org/reference/index.html
 
 
-# load required packages
+# load required packages (if you are using R locally, install them first)
 library(tidyverse)
 library(rcis)
 
-# view header of the dataset
+# view header of the dataset, look at the variables (and their names)
 scorecard
 glimpse(scorecard)
+
+# compare head() with glimpse()
+head(scorecard)
 
 # view help file for the dataset
 ?scorecard
@@ -19,33 +22,24 @@ glimpse(scorecard)
 filter(.data = ________, ________)
 
 
-# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
-# hint: use arrange and slice to sort the data from most to least expensive (desc), keep the first 10
-arrange(.data = ________, ________) %>%
-  slice(________)
-# alternative solution using slice_max() function
-slice_max(.data = ________, order_by = ________, n = ________)
-
-
 # generate a data frame with the average SAT score for each type of college
 scorecard %>%
   group_by(________) %>%
-  summarize(mean_sat = ______________________)
+  summarize(mean_sat = _________(___________))
 
 
-# calculate for each school how many students it takes to pay the average faculty
-# member's salary and generate a data frame with the school's name and the calculated value
+# generate a data frame with the 10 most expensive (netcost) colleges in 2018-19
+# HINT: use arrange(), sort the data from most to least expensive, keep the first 10
+arrange(.data = ________, desc(________)) %>%
+  ______(________)
+# alternative solution using slice_max()
+slice_max(.data = ________, order_by = ________, n = ________)
+
+
+# for each school calculate how many students it takes to pay the average faculty salary
+# and generate a data frame with the school's name and the calculated value
 # use net cost of attendance
 scorecard %>%
   mutate(________ = _______ / _________) %>%
   select(________, ________)
 
-
-# Bonus: calculate how many private, nonprofit schools have a smaller net cost
-# than the "University of Chicago": total number of schools and percentage.
-# hint: the result should be a data frame with one row for the "University of Chicago",
-# and a column containing the requested value.
-
-## code for the number as the total number of schools
-
-## code for the number as the percentage of schools
